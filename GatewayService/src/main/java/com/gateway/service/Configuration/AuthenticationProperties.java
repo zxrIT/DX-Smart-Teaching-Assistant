c0762329c3bb5ea4e4ac5f8e0855879a3543e01a
zxrIT <zengxiangruiit@yeah.net>
2025-06-19 21:01:50 +0800

    基础环境搭建结束

---
+++ GatewayService/src/main/java/com/gateway/service/Configuration/AuthenticationProperties.java
@@ -0,0 +1,16 @@
+package com.gateway.service.Configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@EnableConfigurationProperties({AuthenticationProperties.class})
+@Configuration
+@ConfigurationProperties(prefix = "dx.smart.teaching.assistant.auth")
+public class AuthenticationProperties {
+    private List<String> excludePaths;
+}

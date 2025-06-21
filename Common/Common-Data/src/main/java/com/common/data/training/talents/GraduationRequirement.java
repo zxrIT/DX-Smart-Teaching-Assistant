1a75259ad14d47dbc84217812060a7b8edaf97cf
wuikun1 <734465641qq@gmail.com>
2025-06-21 17:19:04 +0800

    简历人培对应的实体类

---
+++ Common/Common-Data/src/main/java/com/common/data/training/talents/GraduationRequirement.java
@@ -0,0 +1,10 @@
+package com.common.data.training.talents;
+import lombok.Data;
+
+@Data
+public class GraduationRequirement {
+    public String graduationRequirementId;
+    public String requirement;
+    public Short serial;
+    public String trainingTalentId;
+}
---
+++ Common/Common-Data/src/main/java/com/common/data/training/talents/ObjectivesTrainingTarget.java
@@ -0,0 +1,10 @@
+package com.common.data.training.talents;
+import lombok.Data;
+
+@Data
+public class ObjectivesTrainingTarget {
+    public String objectivesTrainingTargetId;
+    public String Target;
+    public String training_talent_id;
+    public Short serial;
+}
